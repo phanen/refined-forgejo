3b8dd98aec43164429fd801df5a54a8cbacfa73f
phanium <91544758+phanen@users.noreply.github.com>
2026-05-06 00:33:32 +0800

    Add fit-textareas feature

---
+++ source/features/fit-textareas.tsx
@@ -0,0 +1,24 @@
+import './fit-textareas.css';
+
+import features from '../feature-manager.js';
+import observe from '../helpers/selector-observer.js';
+
+function watchTextarea(textarea: Element): void {
+	if (!(textarea instanceof HTMLTextAreaElement)) {
+		return;
+	}
+	if (textarea.classList.contains('rgh-fit-textareas')) {
+		return;
+	}
+
+	textarea.classList.add('rgh-fit-textareas');
+	textarea.removeAttribute('rows');
+}
+
+function init(signal: AbortSignal): void {
+	observe('textarea', watchTextarea, {signal});
+}
+
+features.add(import.meta.url, {
+	init,
+});
--- source/refined-forgejo.ts
+++ source/refined-forgejo.ts
@@ -1,3 +1,4 @@
 import './features/reactions-avatars.js';
 import './features/sort-conversations-by-update-time.js';
 import './features/tab-to-indent.js';
+import './features/fit-textareas.js';
