bbac38be0bb758c913f1484d6d40bfc50651e895
phanium <91544758+phanen@users.noreply.github.com>
2026-05-11 20:55:30 +0800

    Fix clean-footer: use observe + click listener instead of delegate

--- source/features/clean-footer.tsx
+++ source/features/clean-footer.tsx
@@ -1,16 +1,24 @@
 import "./clean-footer.css";
 
-import delegate from "delegate-it";
-
 import features from "../feature-manager.js";
+import observe from "../helpers/selector-observer.js";
 
-function init(signal: AbortSignal): void {
-  delegate(".page-footer", "click", () => {
-    document.querySelector(".page-footer")?.classList.toggle("rgf-footer-expanded");
+function handleClick(footer: Element, { signal }: { signal?: AbortSignal }): void {
+  footer.addEventListener("click", () => {
+    footer.classList.toggle("rgf-footer-expanded");
   }, { signal });
 }
 
-void features.addCssFeature(import.meta.url);
+function init(signal: AbortSignal): void {
+  observe(".page-footer", handleClick, { signal });
+}
+
 features.add(import.meta.url, {
   init,
 });
+
+/*
+Test URLs:
+
+- https://codeberg.org/ziglang/zig
+*/
