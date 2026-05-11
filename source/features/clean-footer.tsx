0e30ee8a83b31248b2cfb878341dfe1bfef19f3d
phanium <91544758+phanen@users.noreply.github.com>
2026-05-11 20:48:08 +0800

    Improve clean-footer: hide by default (opacity 0.1), show on hover
    Footer is barely visible with opacity 0.1 until hovered, matching
    the user's request to make the large footer area unobtrusive.

---
+++ source/features/clean-footer.tsx
@@ -0,0 +1,11 @@
+import "./clean-footer.css";
+
+import features from "../feature-manager.js";
+
+void features.addCssFeature(import.meta.url);
+
+/*
+Test URLs:
+
+- https://codeberg.org/ziglang/zig
+*/
