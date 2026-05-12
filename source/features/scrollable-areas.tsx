294857cb89735a6ce362faf5a3aa032e67e1a03d
phanium <91544758+phanen@users.noreply.github.com>
2026-05-12 11:50:53 +0800

    Implement scrollable-areas with click-to-expand
    Port from upstream: limit blockquote/pre height to 30.5em with
    scroll; click to expand fully. Targets Forgejo's .comment-body
    and .markup containers. Use rgf- naming prefix.

--- source/features/scrollable-areas.tsx
+++ source/features/scrollable-areas.tsx
@@ -1,14 +1,37 @@
 import "./scrollable-areas.css";
 
+import delegate, { type DelegateEvent } from "delegate-it";
+
 import features from "../feature-manager.js";
 
+const scrollableSelector = [
+  ".comment-body blockquote",
+  ".comment-body pre",
+  ".markup blockquote",
+  ".markup pre",
+].join(",");
+
+function disableScroll(event: DelegateEvent<MouseEvent, HTMLElement>): void {
+  const area = event.delegateTarget;
+  if (area.scrollHeight <= area.clientHeight) {
+    return;
+  }
+
+  window.scrollBy(0, area.scrollTop);
+  area.classList.add("rgf-scrollable-expanded");
+}
+
+function init(signal: AbortSignal): void {
+  delegate(scrollableSelector, "click", disableScroll, { signal });
+}
+
+void features.addCssFeature(import.meta.url);
 features.add(import.meta.url, {
-  init() {},
+  init,
 });
 
 /*
 Test URLs:
 
-- https://codeberg.org/ziglang/zig
-- https://codeberg.org/ziglang/zig/issues
+- https://codeberg.org/ziglang/zig/issues/1
 */
