18119b6556b091d18ca6637e8473b5aa3dd0c234
phanium <91544758+phanen@users.noreply.github.com>
2026-05-14 20:33:52 +0800

    releases-tab: only when hasRepoHeader

--- source/features/releases-tab.tsx
+++ source/features/releases-tab.tsx
@@ -1,12 +1,14 @@
 import features from "../feature-manager.js";
 import { buildRepoUrl } from "../forgejo-helpers/index.js";
 import { registerHotkey } from "../github-helpers/hotkey.js";
+import { hasRepoHeader } from "../helpers/page-detect.js";
 
 function init(signal: AbortSignal): void {
   registerHotkey("g r", buildRepoUrl("releases"), { signal });
 }
 
 void features.add(import.meta.url, {
+  include: [hasRepoHeader],
   shortcuts: {
     "g r": "Go to Releases",
   },
