630180444becfb6b6c81340c18ce13bd14bdbd10
phanium <91544758+phanen@users.noreply.github.com>
2026-05-14 12:17:44 +0800

    Remove emphasize-draft-pr-label as no need

--- source/refined-forgejo.ts
+++ source/refined-forgejo.ts
@@ -28,7 +28,6 @@ import "./features/repo-header-info.js";
 import "./features/small-user-avatars.js";
 import "./features/quick-file-edit.js";
 import "./features/tab-size.js";
-import "./features/emphasize-draft-pr-label.js";
 import "./features/clean-pinned-issues.js";
 import "./features/conversation-authors.js";
 import "./features/clean-conversation-filters.js";
